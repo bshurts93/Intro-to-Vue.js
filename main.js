d5ed67cfd5a4bd94e506555b200d6a0b1cdf7819
Ben Shurts <bshurts93@gmail.com>
2019-06-26 16:15:06 -0500

    Custom form validation added as part of the onSubmit method

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+Vue.config.devtools = true;
+
 Vue.component("product", {
   props: {
     premium: Boolean,
@@ -34,6 +36,20 @@ Vue.component("product", {
       Add to cart
     </button>
   </div>
+
+  <div>
+    <h2>Reviews</h2>
+    <p v-if="!reviews.length">There are no reviews yet.</p>
+    <ul>
+      <li v-for="review in reviews">
+        <p>{{ review.name }}</p>
+        <p>Review: {{ review.rating }}</p>
+        <p>{{ review.review }}</p>
+      </li>
+    </ul>
+  </div>
+
+  <product-review @review-submitted="addReview"></product-review>
 </div>
   `,
   data() {
@@ -50,7 +66,6 @@ Vue.component("product", {
       ],
       cart: 0,
       onSale: true,
-
       variants: [
         {
           variantId: 2234,
@@ -64,7 +79,8 @@ Vue.component("product", {
           variantImage: "./assets/vmSocks-blue-onWhite.jpg",
           variantQuantity: 0
         }
-      ]
+      ],
+      reviews: []
     };
   },
   methods: {
@@ -73,6 +89,9 @@ Vue.component("product", {
     },
     updateProduct(index) {
       this.selectedVariant = index;
+    },
+    addReview(productReview) {
+      this.reviews.push(productReview);
     }
   },
   computed: {
@@ -95,6 +114,73 @@ Vue.component("product", {
   }
 });
 
+Vue.component("product-review", {
+  template: `
+  <form class="review-form" @submit.prevent="onSubmit">
+
+  <p v-if="errors.length">
+    <b>Please correct the following errors.
+    <ul id="form-errors">
+      <li v-for="error in errors">{{error}}</li>
+    </ul>
+  </p>
+
+  <p>
+    <label for="name">Name:</label>
+    <input id="name" v-model="name" />
+  </p>
+
+  <p>
+    <label for="review">Review:</label>
+    <textarea id="review" v-model="review"></textarea>
+  </p>
+
+  <p>
+    <label for="rating">Rating:</label>
+    <select id="rating" v-model.number="rating">
+      <option>5</option>
+      <option>4</option>
+      <option>3</option>
+      <option>2</option>
+      <option>1</option>
+    </select>
+  </p>
+
+  <p>
+    <input type="submit" value="submit">
+  </p>
+</form>
+  `,
+  data() {
+    return {
+      name: null,
+      review: null,
+      rating: null,
+      errors: []
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.errors = [];
+      if (this.name && this.review && this.rating) {
+        let productReview = {
+          name: this.name,
+          review: this.review,
+          rating: this.rating
+        };
+        this.$emit("review-submitted", productReview);
+        this.name = null;
+        this.review = null;
+        this.rating = null;
+      } else {
+        if (!this.name) this.errors.push("Name required.");
+        if (!this.review) this.errors.push("Review required.");
+        if (!this.rating) this.errors.push("Rating required.");
+      }
+    }
+  }
+});
+
 var app = new Vue({
   el: "#app",
   data: {
@@ -107,5 +193,3 @@ var app = new Vue({
     }
   }
 });
-
-Vue.config.devtools = true;
